1d2385376feb7e916b2eea4cf43204ae74918f87
Gangadhar <venkatagangadhararao9@gmail.com>
2025-10-30 18:50:12 +0530

    commit 3

--- day-4/backend.tf
+++ day-4/backend.tf
@@ -2,9 +2,9 @@ terraform {
   backend "s3" {
     bucket = "gangaterraformnit"
     key    = "terraform.tfstate"
-    use_lockfile = true # to use s3 native locking 1.19 version above
+    #use_lockfile = true # to use s3 native locking 1.19 version above
     region = "ap-south-1"
-    #dynamodb_table = "Ganga" # any version we can use dynamodb locking 
-    #encrypt = true
+    dynamodb_table = "Ganga" # any version we can use dynamodb locking 
+    encrypt = true
   }
 }
