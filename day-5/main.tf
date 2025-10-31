2475484faf71de4cd9efae5ad6a4676e63d7a86c
Gangadhar <venkatagangadhararao9@gmail.com>
2025-10-31 17:34:18 +0530

    custom-network-creation

---
+++ day-5/main.tf
@@ -0,0 +1,158 @@
+resource "aws_vpc" "name" {
+  cidr_block = "10.0.0.0/16"
+  region = "ap-south-1"
+  tags = {
+    Name = "Day-5-vpc"
+  }
+}
+
+resource "aws_subnet" "name1" {
+  cidr_block = "10.0.0.0/20"
+  vpc_id = aws_vpc.name.id
+  tags = {
+    Name = "subnet-1"
+  }
+}
+
+resource "aws_subnet" "name2" {
+  cidr_block = "10.0.16.0/20"
+  vpc_id = aws_vpc.name.id
+  tags = {
+    Name = "subnet-2"
+  }
+}
+
+resource "aws_subnet" "name3" {
+  cidr_block = "10.0.32.0/20"
+  vpc_id = aws_vpc.name.id
+  tags = {
+    Name = "subnet-3"
+  }
+}
+
+resource "aws_subnet" "name4" {
+  cidr_block = "10.0.48.0/20"
+  vpc_id = aws_vpc.name.id
+  tags = {
+    Name = "subnet-4"
+  }
+}
+
+resource "aws_internet_gateway" "name" {
+  vpc_id = aws_vpc.name.id
+  tags = {
+    Name = "Terra_ig"
+  }
+}
+
+resource "aws_eip" "name" {
+    domain = "vpc"
+
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "name" {
+  subnet_id = aws_subnet.name1.id
+  allocation_id = aws_eip.name.id
+  tags = {
+    Name = "Terra_nat"
+  }
+}
+
+resource "aws_route_table" "pub" {
+  vpc_id = aws_vpc.name.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.name.id
+  }
+  
+  tags = {
+    Name = "public_rt"
+  }
+}
+
+resource "aws_route_table_association" "pub1" {
+  subnet_id = aws_subnet.name1.id
+  route_table_id = aws_route_table.pub.id
+}
+
+resource "aws_route_table_association" "pub2" {
+  subnet_id = aws_subnet.name2.id
+  route_table_id = aws_route_table.pub.id
+}
+
+resource "aws_route_table" "pri" {
+  vpc_id = aws_vpc.name.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.name.id
+  }
+  tags = {
+    Name = "private_rt"
+  }
+}
+
+resource "aws_route_table_association" "pri1" {
+  subnet_id = aws_subnet.name3.id
+  route_table_id = aws_route_table.pri.id
+}
+
+resource "aws_route_table_association" "pri2" {
+  subnet_id = aws_subnet.name4.id
+  route_table_id = aws_route_table.pri.id
+}
+ resource "aws_security_group" "name" {
+   vpc_id = aws_vpc.name.id
+   ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+
+   ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+
+   ingress {
+    description = "MySql"
+    from_port = 3036
+    to_port = 3036
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ }
+
+resource "aws_instance" "pub" {
+  ami = "ami-01760eea5c574eb86"
+  subnet_id = aws_subnet.name1.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.name.id]
+  associate_public_ip_address = true
+  tags = {
+    Name = "public instance"
+  }
+}
+
+resource "aws_instance" "pri" {
+  ami = "ami-01760eea5c574eb86"
+  subnet_id = aws_subnet.name3.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.name.id]
+  tags = {
+    Name = "private instance"
+  }
+}
